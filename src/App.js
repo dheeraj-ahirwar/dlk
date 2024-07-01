9abd6ba73503995e25bcf27c56581fa574c691de
Dheeraj Ahirwar <133276649+dheeraj-ahirwar@users.noreply.github.com>
2024-07-01 21:22:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tracks, setTracks] = useState([]);
+  const [keyword, setKeyword] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getTracks = async () => {
+    setIsLoading(true);
+    let data = await fetch(
+      `https://v1.nocodeapi.com/vijayraj/spotify/tCYEzUHTkeEzFWLi/search?q=${
+        keyword === "" ? "trending" : keyword
+      }&type=track`
+    );
+    let convertedData = await data.json();
+    console.log(convertedData);
+    setTracks(convertedData.tracks.items);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getTracks();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <h2>V-music</h2>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div
+            className="collapse navbar-collapse d-flex justify-content-center"
+            id="navbarSupportedContent"
+          >
+            <input
+              value={keyword}
+              onChange={(event) => setKeyword(event.target.value)}
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button onClick={getTracks} className="btn btn-outline-success">
+              Search
+            </button>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container">
+        <div className={`row ${isLoading ? "" : "d-none"}`}>
+          <div className="col-12 py-5 text-center">
+            <div
+              className="spinner-border"
+              style={{ width: "3rem", height: "3rem" }}
+              role="status"
+            >
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {tracks.map((element) => {
+            return (
+              <div key={element.id} className="col-lg-3 col-md-6 py-2">
+                <div className="card">
+                  <img
+                    src={
+                      element.album.images[0]?.url || "default-image-url.jpg"
+                    }
+                    className="card-img-top"
+                    alt={element.name}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{element.name}</h5>
+                    <p className="card-text">
+                      Artist: {element.album.artists[0].name}
+                    </p>
+                    <p className="card-text">
+                      Release date: {element.album.release_date}
+                    </p>
+                    <audio
+                      src={element.preview_url}
+                      controls
+                      className="w-100"
+                    ></audio>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
